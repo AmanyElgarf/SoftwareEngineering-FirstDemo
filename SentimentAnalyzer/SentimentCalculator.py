df08090d2644faca41e809f6b4efd7496a78fbda
Amany Elgarf <35881779+AmanyElgarf@users.noreply.github.com>
2019-03-27 12:57:49 -0400

    Update SentimentCalculator.py

--- SentimentAnalyzer/SentimentCalculator.py
+++ SentimentAnalyzer/SentimentCalculator.py
@@ -3,7 +3,9 @@ import re
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 class SentimentCalculator:
-    def __init__(self):
+    
+    #The constructor has two attributes, newsAnalyzer and financeLexicon.
+    def __init__(self):  
         self.newsAnalyzer = SentimentIntensityAnalyzer()
         self.financeLexicon = {}
         with open("FinanceLexicon.txt") as dictFile:
@@ -12,16 +14,8 @@ class SentimentCalculator:
                 (word, score) = line.split(',')
                 self.financeLexicon[word] = int(eval(score))
         self.newsAnalyzer.lexicon.update(self.financeLexicon)
-    def calculate(self, message):
-        #message = "Alphabet (GOOGL) Dips More Than Broader Markets: What You Should Know"
-        #isValid = 0
-        #regex = re.compile("(\w[\w']*\w|\w)")
-        #for msgWord in regex.findall(message):
-         #   for lexWord in self.newsAnalyzer.lexicon:
-          #      if (msgWord == lexWord):
-           #         isValid = 1
-            #        break
-        #if (isValid == 0):
-         #   return 2.0 #return 2 on error/none of the numbers match up
+        
+    #This function takes a string headline as an input, analyzes it sentiment score, and returns that score
+    def calculate(self, message): 
         ss = self.newsAnalyzer.polarity_scores(message)
-        return ss["compound"] #otherwise, we know that at least one word in lexicon is in the input text and can assign proper score
+        return ss["compound"] 
