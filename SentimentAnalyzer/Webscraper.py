8441b216ab844857c46eaa29cbc0bfaf7eca6466
AndreaDumalagan <43146256+AndreaDumalagan@users.noreply.github.com>
2019-03-27 10:39:40 -0400

    Update Webscraper.py
    Updated comments.

--- SentimentAnalyzer/Webscraper.py
+++ SentimentAnalyzer/Webscraper.py
@@ -13,16 +13,18 @@ class Webscraper:
     
     #getHeadlines() function
     def getHeadlines(self, ticker):
+        #utilizes News API
+            #grabs headlines from, and inclusive of, 5 days prior user search
         self.url = ('https://newsapi.org/v2/everything?'
                 'q=' + ticker + '&'
                 'from=' + str(date.today() + timedelta(days=-5)) + '&'
                 'sortBy=popularity&'
                 'apiKey=22d271b54d1845858fbddf96cb9d20d2')
-
+        #creates .json file of all the headlines grabbed
         self.response = requests.get(self.url).json()
         for article in self.response["articles"]:
             self.headlines.append(article["title"])
-        return self.headlines
+        return self.headlines 
         
         #TESTING:
         
